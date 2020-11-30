6e6b56a210f930e434ee3bdb3cac3630b94d7c91
georgethedeveloper77 <georgekarani77@gmail.com>
2020-11-30 12:24:22 +0300

    echo "bascis";

---
+++ Readme.md
@@ -0,0 +1,36 @@
+**Web Server**
+
+php -S localhost:4000 /
+
+
+**Create a Virtual Host for your Website**
+1. sudo mkdir /var/www/your_domain
+2. sudo chown -R $USER:$USER /var/www/your_domain
+3. sudo nano /etc/apache2/sites-available/your_domain.conf
+* /etc/apache2/sites-available/your_domain.conf
+<VirtualHost *:80>
+    ServerName your_domain
+    ServerAlias www.your_domain
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/your_domain
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+1. sudo a2ensite your_domain
+2. sudo a2dissite 000-default
+3. sudo apache2ctl configtest
+4. sudo systemctl reload apache2
+
+Restart apache server
+/etc/init.d/apache2 restart
+
+# Commands
+**echo**
+
+## Variables
+containers
+
+
+
+
+
---
+++ site.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>30DaysOfPHP</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+  <?php
+  $name = "George";
+  $age = 30;
+
+  echo "There once wa a man named $name <br>";
+  echo "He was $age years old <br>";
+  echo "He really liked the name $name<br>";
+  echo "but didn't liked being $age years old <br>";
+
+
+
+  $name = "Bloom";
+  $age = 22;
+
+  echo "<br>There once wa a man named $name <br>";
+  echo "He was $age years old <br>";
+  echo "He really liked the name $name<br>";
+  echo "but didn't liked being $age years old <br>";
+  ?>
+
+
+  <script src="" async defer>
+  </script>
+</body>
+
+</html>
