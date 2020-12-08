df37c468d14b3a42c43e34d469be3758287798e5
georgethedeveloper77 <georgekarani77@gmail.com>
2020-12-08 17:27:37 +0300

    echo date( 'Y-m-d H:i:s'). br

--- PHPCourse2020/09_dates.php
+++ PHPCourse2020/09_dates.php
@@ -7,7 +7,7 @@ echo time() . '<br>';
 echo date('Y-m-d H:i:s') . '<br>';
 
 // 3. Print yesterday
-echo date('Y-m-d H:i:s', time() - 60 * 60 * 24) . '<br>';
+echo date('Y-m-d H:i:s', time() - 60 * 60 * 24) . '<br>';  //- 60secs *60(hr)
 
 // 4. Different format: https://www.php.net/manual/en/function.date.php
 echo date('F j Y, H:i:s') . '<br>';
@@ -18,7 +18,7 @@ echo strtotime('+1 day') . "<br>";
 echo strtotime('+1 week') . "<br>";
 
 // 6. Parse date: https://www.php.net/manual/en/function.date-parse.php
-$dateString = '2020-02-06 12:45:35'; // Declare date
+$dateString = '2020-12-08 17:25:35'; // Declare date
 $parsedDate = date_parse($dateString); // Parse date
 echo '<pre>';
 var_dump($parsedDate);
