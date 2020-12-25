92a811b9630a0c67daf66da0c6eca30d1985092c
georgethedeveloper <georgekarani77@gmail.com>
2020-12-25 03:11:05 +0300

    varibles

--- PHP C DEC 2020/02_variables.php
+++ PHP C DEC 2020/02_variables.php
@@ -2,24 +2,47 @@
 
 // What is a variable
 
-// Variable types
 
+// Variable types..String Integer Float Boolean Null Array Object Resource
+//$name = "George";
+//$age = 23;
 // Declare variables
+$name = "George";
+$age = 23;
+$isMale = true;
+$height = 1.85;
+$salary = null;
 
 // Print the variables. Explain what is concatenation
+echo $name . '<br>';
+echo $age . '<br>';
+echo $isMale . '<br>';
 
 // Print types of the variables
+echo gettype($name) . '<br>';
 
 // Print the whole variable
+var_dump($name, $age, $salary, $height) . '<br><br>';
 
 // Change the value of the variable
-
+echo "<br>";
+$name = false;
 // Print type of the variable
+echo gettype($name) . '<br>';
 
 // Variable checking functions
+is_string(name);
+is_bool($isMale);
 
 // Check if variable is defined
+isset($name); ///true
+isset($address); //false
+
 
 // Constants
+define('PI', 3.14);
+echo PI.'<br>';
 
 // Using PHP built-in constants
+echo SORT_ASC .'<br>';
+echo PHP_INT_MAX . '<br>';
