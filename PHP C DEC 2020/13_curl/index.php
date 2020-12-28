a3ff3e149c1f310e5535c3cb7a1346aca030dca8
georgethedeveloper <georgekarani77@gmail.com>
2020-12-28 16:31:31 +0300

    Products crud...bad/beginner versions

--- PHP C DEC 2020/13_curl/index.php
+++ PHP C DEC 2020/13_curl/index.php
@@ -2,9 +2,29 @@
 
 $url = 'https://jsonplaceholder.typicode.com/users';
 // Sample example to get data.
+//$resource = curl_init($url);
+//curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+//$usersJSON = curl_exec($resource);
+//echo $usersJSON;
+//curl_close($resource);
+//
+//// Get response status code
+//$responseCode = curl_getinfo($resource, CURLINFO_HTTP_CODE);
+//echo $responseCode;
 
-// Get response status code
+// Create User
+$user = [
+    'name' => 'John Doe',
+    'username' => 'john',
+    'email' => 'john@example.com'
+];
 
-// set_opt_array
-
-// Post request
+$ch = curl_init($url);
+curl_setopt_array($ch, [
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => json_encode($user),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => array('Content-Type: application/json')
+]);
+$result = curl_exec($ch);
+curl_close($ch);
